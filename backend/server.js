dfc1bca20ceabd56a243e63406d6bcc6fb4514a7
GaganChandan2003 <gaganchandan43@gmail.com>
2024-06-13 16:36:31 +0530

    Final setup done

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connection from "../backend/config/db.js";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
 
+const __dirname = path.resolve();
 dotenv.config();
 
 app.use(express.json());
@@ -16,8 +18,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(process.env.PORT || 8000, async () => {
